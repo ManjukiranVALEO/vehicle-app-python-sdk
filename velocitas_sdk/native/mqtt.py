08e9f49d97662def2b6495cf39f286019da8eb73
Ravi Prasad B S <raviprasad-b.shivarama@valeo.com>
2025-02-05 13:57:25 +0530

    updated mqtt tls_set parameters

--- velocitas_sdk/native/mqtt.py
+++ velocitas_sdk/native/mqtt.py
@@ -44,8 +44,8 @@ class MqttClient(PubSubClient):
         self._pub_client = mqtt.Client()
         self._sub_client = mqtt.Client()
         if self._cacert is not None and self._key is not None and self._device_cert is not None:
-            self._pub_client.tls_set(self._cacert, self._key, self._device_cert)
-            self._sub_client.tls_set(self._cacert, self._key, self._device_cert)
+            self._pub_client.tls_set(cacerts=self._cacert, certfile=self._device_cert, keyfile=self._key)
+            self._sub_client.tls_set(cacerts=self._cacert, certfile=self._device_cert, keyfile=self._key)
         self._sub_client.on_connect = self.on_connect
         self._sub_client.on_disconnect = self.on_disconnect
 
